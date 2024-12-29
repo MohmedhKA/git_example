3dc824f7e76bf34b3420079bb45d2948886414ff
MohmedhKA <kamohmedh2006@gmail.com>
2024-12-29 12:34:52 +0530

    inserted with vi - new changes

--- src/gitTestingSpot/HelloWorld.java
+++ src/gitTestingSpot/HelloWorld.java
@@ -2,6 +2,7 @@ package gitTestingSpot;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		
+		System.out.println("Hello World!");
 	}
 }
